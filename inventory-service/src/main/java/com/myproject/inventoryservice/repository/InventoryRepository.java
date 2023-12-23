94974f3b9ae61d694e2c4cee3d1c4031ed280356
Shivam Saini <76945575+Shivam-Saini-SS@users.noreply.github.com>
2023-12-24 01:55:22 +0530

    Initial Commit with Maven Multi Module Project
    - Created inventory-service
    - Created order-service
    - Created product-service

---
+++ inventory-service/src/main/java/com/myproject/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.myproject.inventoryservice.repository;
+
+import com.myproject.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory,Long> {
+    Optional<Inventory> findBySkuCode(String skuCode);
+}
