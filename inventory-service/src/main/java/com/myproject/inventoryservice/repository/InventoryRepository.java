5a1e43408644b9531cc66f97c5b0ab3c2a94ee88
Shivam Saini <76945575+Shivam-Saini-SS@users.noreply.github.com>
2023-12-24 21:30:03 +0530

    Enabled Inter-service Communication and Service Discovery

--- inventory-service/src/main/java/com/myproject/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/myproject/inventoryservice/repository/InventoryRepository.java
@@ -3,8 +3,8 @@ package com.myproject.inventoryservice.repository;
 import com.myproject.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface InventoryRepository extends JpaRepository<Inventory,Long> {
-    Optional<Inventory> findBySkuCode(String skuCode);
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
 }
--- order-service/src/main/java/com/myproject/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/myproject/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.myproject.orderservice.service;
 
+import com.myproject.orderservice.dto.InventoryResponse;
 import com.myproject.orderservice.dto.OrderLineItemsDto;
 import com.myproject.orderservice.dto.OrderRequest;
 import com.myproject.orderservice.model.Order;
@@ -8,7 +9,9 @@ import com.myproject.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,7 +19,10 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @Transactional
 public class OrderService {
+
     private final OrderRepository orderRepository;
+    private final WebClient.Builder webClientBuilder;
+
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -28,7 +34,27 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItemsList);
 
-        orderRepository.save(order);
+        List<String> skuCodes = order.getOrderLineItemsList().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+        // Call Inventory Service and place the order
+        // if the product is in stock
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                        .uri("http://inventory-service/api/inventory",
+                                uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                        .retrieve()
+                        .bodyToMono(InventoryResponse[].class)
+                        .block();
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponses)
+                                    .allMatch(InventoryResponse::isInStock);
+
+        if (allProductsInStock) {
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        }
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
